6274ca4df37751e6e82514271e0d578fb8641b5c
Thinkofdeath <thinkofdeath@spigotmc.org>
2015-04-04 22:13:49 +0100

    /: support x and z rotation

--- blockmodelrender.go
+++ blockmodelrender.go
@@ -129,6 +129,22 @@ func precomputeModel(bm *blockModel) *processedModel {
 						z := float64(vert[v].Z) - r.origin[2]*16
 						vert[v].X = int16(r.origin[0] + (x*c - z*s))
 						vert[v].Z = int16(r.origin[2] + (z*c + x*s))
+					case "x":
+						rotX := r.angle * (math.Pi / 180)
+						c := math.Cos(-rotX)
+						s := math.Sin(-rotX)
+						z := float64(vert[v].Z) - r.origin[1]*16
+						y := float64(vert[v].Y) - r.origin[1]*16
+						vert[v].Z = int16(r.origin[2] + (z*c - y*s))
+						vert[v].Y = int16(r.origin[1] + (y*c + z*s))
+					case "z":
+						rotZ := r.angle * (math.Pi / 180)
+						c := math.Cos(-rotZ)
+						s := math.Sin(-rotZ)
+						x := float64(vert[v].X) - r.origin[0]*16
+						y := float64(vert[v].Y) - r.origin[1]*16
+						vert[v].X = int16(r.origin[0] + (x*c - y*s))
+						vert[v].Y = int16(r.origin[1] + (y*c + x*s))
 					}
 				}
 
