ab2dacac1c5e10c035274d5907baf4fbe2efcc38
Thinkofdeath <thinkofdeath@spigotmc.org>
2015-04-23 00:37:11 +0100

    steven: allow for all types of images for biome colors

--- blocktint.go
+++ blocktint.go
@@ -17,6 +17,7 @@ package steven
 import (
 	"fmt"
 	"image"
+	icolor "image/color"
 	"image/png"
 
 	"github.com/thinkofdeath/steven/resource"
@@ -39,5 +40,27 @@ func loadBiomeColors(name string) *image.NRGBA {
 	if err != nil {
 		panic(err)
 	}
-	return img.(*image.NRGBA)
+	i, ok := img.(*image.NRGBA)
+	if !ok {
+		i = convertImage(img)
+	}
+	return i
+}
+
+func convertImage(img image.Image) *image.NRGBA {
+	width, height := img.Bounds().Dx(), img.Bounds().Dy()
+	pix := make([]byte, width*height*4)
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			col := icolor.NRGBAModel.Convert(img.At(x, y)).(icolor.NRGBA)
+			index := (y*width + x) * 4
+			pix[index] = col.R
+			pix[index+1] = col.G
+			pix[index+2] = col.B
+			pix[index+3] = col.A
+		}
+	}
+	i := image.NewNRGBA(img.Bounds())
+	i.Pix = pix
+	return i
 }
