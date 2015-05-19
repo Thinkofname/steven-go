57b4cfe7fdf646aa855cd190116baabda45071e9
Thinkofdeath <thinkofdeath@spigotmc.org>
2015-05-19 22:16:11 +0100

    steven: add missing sword types

--- itemlist.go
+++ itemlist.go
@@ -15,33 +15,40 @@
 package steven
 
 var itemsByID = map[int]func() ItemType{
+	267: func() ItemType {
+		i := &itemSword{}
+		i.locale = "item.swordIron.name"
+		i.itemNamed.name = "iron_sword"
+		i.maxDamage = 251
+		return i
+	},
+	268: func() ItemType {
+		i := &itemSword{}
+		i.locale = "item.swordWood.name"
+		i.itemNamed.name = "wooden_sword"
+		i.maxDamage = 260
+		return i
+	},
+	272: func() ItemType {
+		i := &itemSword{}
+		i.locale = "item.swordStone.name"
+		i.itemNamed.name = "stone_sword"
+		i.maxDamage = 132
+		return i
+	},
 	276: func() ItemType {
-		i := &itemSword{
-			itemSimpleLocale: itemSimpleLocale{
-				locale: "item.swordDiamond.name",
-			},
-			itemNamed: itemNamed{
-				name: "diamond_sword",
-			},
-			itemDamagable: itemDamagable{
-				maxDamage: 1562,
-			},
-		}
+		i := &itemSword{}
+		i.locale = "item.swordDiamond.name"
+		i.itemNamed.name = "diamond_sword"
+		i.maxDamage = 1562
 		return i
 	},
 
 	283: func() ItemType {
-		i := &itemSword{
-			itemSimpleLocale: itemSimpleLocale{
-				locale: "item.swordGold.name",
-			},
-			itemNamed: itemNamed{
-				name: "golden_sword",
-			},
-			itemDamagable: itemDamagable{
-				maxDamage: 33,
-			},
-		}
+		i := &itemSword{}
+		i.locale = "item.swordGold.name"
+		i.itemNamed.name = "golden_sword"
+		i.maxDamage = 33
 		return i
 	},
 }
