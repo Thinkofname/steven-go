51fb2a415c75987487d20331d306615ff0b10bfc
Thinkofdeath <thinkofdeath@spigotmc.org>
2015-08-17 20:54:24 +0100

    protocol: update for metadata changes

--- protocol/metadata.go
+++ protocol/metadata.go
@@ -16,8 +16,12 @@ package protocol
 
 import (
 	"encoding/binary"
+	"encoding/json"
 	"errors"
 	"io"
+	"strconv"
+
+	"github.com/thinkofdeath/steven/format"
 )
 
 // Metadata is a simple index -> value map used in the Minecraft protocol.
@@ -30,51 +34,86 @@ import (
 //     ItemStack
 //     []int32
 //     []float32
+//     VarInt
+//     UUID
+//     format.AnyComponent
 type Metadata map[int]interface{}
 
 func readMetadata(r io.Reader) (Metadata, error) {
 	m := make(Metadata)
 	for {
-		b, err := ReadByte(r)
-		if err != nil || b == 0x7F {
+		id, err := ReadByte(r)
+		if err != nil || id == 0xFF {
 			return m, err
 		}
-		index := int(b & 0x1F)
-		t := b >> 5
-
+		index := int(id)
+		t, err := ReadByte(r)
+		if err != nil {
+			return m, err
+		}
 		switch t {
 		case 0:
 			var val int8
 			err = binary.Read(r, binary.BigEndian, &val)
 			m[index] = val
 		case 1:
-			var val int16
-			err = binary.Read(r, binary.BigEndian, &val)
-			m[index] = val
+			var val VarInt
+			val, err = ReadVarInt(r)
+			m[index] = int(val)
 		case 2:
-			var val int32
-			err = binary.Read(r, binary.BigEndian, &val)
-			m[index] = val
-		case 3:
 			var val float32
 			err = binary.Read(r, binary.BigEndian, &val)
 			m[index] = val
-		case 4:
+		case 3:
 			m[index], err = ReadString(r)
+		case 4:
+			str, err := ReadString(r)
+			if err != nil {
+				return m, err
+			}
+			var msg format.AnyComponent
+			err = json.Unmarshal([]byte(str), &msg)
+			m[index] = msg
 		case 5:
 			i := ItemStack{}
 			err = i.Deserialize(r)
 			m[index] = i
 		case 6:
-			var val [3]int32
-			err = binary.Read(r, binary.BigEndian, &val)
-			m[index] = val
+			m[index], err = ReadBool(r)
 		case 7:
 			var val [3]float32
 			err = binary.Read(r, binary.BigEndian, &val)
 			m[index] = val
+		case 8:
+			var val Position
+			err = binary.Read(r, binary.BigEndian, &val)
+			m[index] = val
+		case 9:
+			var ok bool
+			ok, err = ReadBool(r)
+			if err != nil {
+				return m, err
+			}
+			pos := new(Position)
+			if ok {
+				err = binary.Read(r, binary.BigEndian, pos)
+			}
+			m[index] = pos
+		case 10: // Direction
+			m[index], err = ReadVarInt(r)
+		case 11:
+			var ok bool
+			ok, err = ReadBool(r)
+			if err != nil {
+				return m, err
+			}
+			uuid := new(UUID)
+			if ok {
+				err = uuid.Deserialize(r)
+			}
+			m[index] = uuid
 		default:
-			err = errors.New("invalid metadata type")
+			err = errors.New("invalid metadata type " + strconv.Itoa(int(t)))
 		}
 		if err != nil {
 			return m, err
@@ -84,48 +123,58 @@ func readMetadata(r io.Reader) (Metadata, error) {
 
 func writeMetadata(w io.Writer, m Metadata) error {
 	for index, v := range m {
-		t := 0
-
-		var val interface{} = v
-		switch v.(type) {
-		case int8:
-			t = 0
-		case int16:
-			t = 1
-		case int32:
-			t = 2
-		case float32:
-			t = 3
-		case string:
-			t = 4
-			val = nil
-		case ItemStack:
-			t = 5
-			val = nil
-		case [3]int32:
-			t = 6
-		case [3]float32:
-			t = 7
-		default:
-			return errors.New("invalid metadata type")
-		}
-		if err := WriteByte(w, byte(index)|(byte(t)<<5)); err != nil {
+		err := WriteByte(w, byte(index))
+		if err != nil {
 			return err
 		}
-		var err error
-		if val != nil {
-			err = binary.Write(w, binary.BigEndian, val)
-		} else {
-			switch v := v.(type) {
-			case string:
-				err = WriteString(w, v)
-			case ItemStack:
-				err = v.Serialize(w)
+		switch v := v.(type) {
+		case int8:
+			WriteByte(w, 0)
+			err = binary.Write(w, binary.BigEndian, v)
+		case int:
+			WriteByte(w, 1)
+			err = WriteVarInt(w, VarInt(v))
+		case float32:
+			WriteByte(w, 2)
+			err = binary.Write(w, binary.BigEndian, v)
+		case string:
+			WriteByte(w, 3)
+			err = WriteString(w, v)
+		case format.AnyComponent:
+			WriteByte(w, 4)
+			val, _ := json.Marshal(v)
+			err = WriteString(w, string(val))
+		case ItemStack:
+			WriteByte(w, 5)
+			v.Serialize(w)
+		case bool:
+			WriteByte(w, 6)
+			err = WriteBool(w, v)
+		case [3]float32:
+			WriteByte(w, 7)
+			err = binary.Write(w, binary.BigEndian, v)
+		case Position:
+			WriteByte(w, 8)
+			err = binary.Write(w, binary.BigEndian, v)
+		case *Position:
+			WriteByte(w, 9)
+			WriteBool(w, v != nil)
+			if v != nil {
+				err = binary.Write(w, binary.BigEndian, v)
+			}
+		case VarInt: // Direction
+			WriteByte(w, 10)
+			err = WriteVarInt(w, v)
+		case *UUID:
+			WriteByte(w, 11)
+			WriteBool(w, v != nil)
+			if v != nil {
+				v.Serialize(w)
 			}
 		}
 		if err != nil {
 			return err
 		}
 	}
-	return WriteByte(w, 0x7F)
+	return WriteByte(w, 0xFF)
 }
